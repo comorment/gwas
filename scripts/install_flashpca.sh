0df2ac84608e65752d9492f94df3cf7dce0c9b4e
Oleksandr Frei <oleksandr.frei@gmail.com>
2020-12-11 11:04:04 +0100

    add flashpca to gwas container (just the binary - not R)

--- scripts/install_flashpca.sh
+++ scripts/install_flashpca.sh
@@ -1,6 +1,4 @@
-
-git clone git://github.com/gabraham/flashpca
-
-
-cd flashpca
-make all
+wget --no-check-certificate https://github.com/gabraham/flashpca/releases/download/v2.0/flashpca_x86-64.gz && \
+    gunzip flashpca_x86-64.gz && \
+    chmod +x flashpca_x86-64 && \
+    cp flashpca_x86-64 /bin
