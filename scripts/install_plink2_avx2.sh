b07d84098d5b76205cf388e5f4f27bd0913cfba4
bayramakdeniz <66322504+bayramakdeniz@users.noreply.github.com>
2020-12-14 02:30:36 +0100

    Update install_plink2_avx2.sh
    updated link

--- scripts/install_plink2_avx2.sh
+++ scripts/install_plink2_avx2.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-wget --no-check-certificate http://s3.amazonaws.com/plink2-assets/plink2_linux_avx2_20201028.zip && \
-    unzip -j plink2_linux_avx2_20201028.zip && \
-    rm -rf plink2_linux_avx2_20201028.zip
+wget --no-check-certificate http://s3.amazonaws.com/plink2-assets/plink2_linux_avx2_20201212.zip && \
+    unzip -j plink2_linux_avx2_20201212.zip && \
+    rm -rf plink2_linux_avx2_20201212.zip
 # we install non-avx plink2 so that default plink runs on any  CPU architecture
-#cp plink2 /bin
+
+
+mv plink2 plink2_avx2
+rm -f plink2
+
+cp plink2_avx2 /bin
