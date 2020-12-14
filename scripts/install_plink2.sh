2c14cca25497494005baf3252e850a51734a9fda
bayramakdeniz <66322504+bayramakdeniz@users.noreply.github.com>
2020-12-14 02:29:33 +0100

    Update install_plink2.sh
    The download link was not working anymore hence I have updated it

--- scripts/install_plink2.sh
+++ scripts/install_plink2.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-wget --no-check-certificate http://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20201028.zip && \
-    unzip -j plink2_linux_x86_64_20201028.zip && \
-    rm -rf plink2_linux_x86_64_20201028.zip
+wget --no-check-certificate http://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20201212.zip && \
+    unzip -j plink2_linux_x86_64_20201212.zip && \
+    rm -rf plink2_linux_x86_64_20201212.zip
+
 cp plink2 /bin
