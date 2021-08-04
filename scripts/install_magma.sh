e93b6c112ddac9c13cdbe554109e8796bf701ecc
Oleksandr Frei <oleksandr.frei@gmail.com>
2021-08-04 09:47:54 +0000

    update gwas.sif and r.sif with new tools

---
+++ scripts/install_magma.sh
@@ -0,0 +1,6 @@
+wget --no-check-certificate https://ctg.cncr.nl/software/MAGMA/prog/magma_v1.09a_static.zip && \
+    unzip magma_v1.09a_static.zip && \
+    chmod +x magma && \
+    cp magma /bin
+
+
