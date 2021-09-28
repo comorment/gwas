dae56531a5db8c91a4c74f23fe50b7c446a9ad05
Oleksandr Frei <oleksandr.frei@gmail.com>
2021-09-28 13:31:43 +0000

    mixer sandybridge

--- scripts/install_mixer.sh
+++ scripts/install_mixer.sh
@@ -1,9 +1,9 @@
 cd /tools
-wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+wget https://boostorg.jfrog.io/artifactory/main/release/1.69.0/source/boost_1_69_0.tar.gz
 tar -xzvf boost_1_69_0.tar.gz && cd boost_1_69_0
 ./bootstrap.sh --with-libraries=program_options,filesystem,system,date_time
-./b2 --clean && ./b2 --j12 -a
+./b2 --clean && ./b2 cflags=-march=sandybridge cxxflags=-march=sandybridge --j12 -a
 cd /tools
-git clone --recurse-submodules -j8 https://github.com/precimed/mixer.git
+git clone --branch sandybridge --recurse-submodules -j8 https://github.com/precimed/mixer.git
 mkdir mixer/src/build && cd mixer/src/build && cmake .. -DBoost_NO_BOOST_CMAKE=ON -DBOOST_ROOT=/tools/boost_1_69_0 && make -j16 bgmg
 	
